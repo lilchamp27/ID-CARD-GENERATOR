d67b0e91febbcb6b49973064f7687f881a0e0619
lilchamp27 <oduneyebolu004@gmail.com>
2024-09-22 19:40:08 +0100

    updated

--- script/main.js
+++ script/main.js
@@ -1,37 +1,72 @@
-// calling the form Id
-let formInput = document.getElementById('formId')
-console.log(formInput)
+const staffDatabase = [];
 
-// calling the ids for the form
-let inputName = document.getElementById('nameInput');
-let inputPosition = document.getElementById('positionInput');
-let inputIdnumber = document.getElementById('idNumberInput');
-let inputJoindate = document.getElementById('joinDateInput');
-let inputExpdate = document.getElementById('expDateInput');
-let inputBlood = document.getElementById('bloodInput');
+// Getting the form element
+let formInput = document.getElementById('FormId');
 
-// Calling the id of the button
 let inputButton = document.getElementById('btn');
 
-function addToarray(e) {
-    e.preventDefault();
-    console.log("form submit")
+formInput.addEventListener("submit", addToArray);
+
+
+function addToArray(e) {
+    e.preventDefault(); 
+
 
     const newArray = {
-       name: formInput.name.value,
-       postion: formInput.postion.value,
-        id : formInput.IdNumber.value,
-         joinDate : formInput.Joineddate.value,
-        expirationDate:  formInput.expDate.value,
-        bloodGroup: formInput.blood.value,
-    
+       name: formInput['name'].value,
+       position: formInput['position'].value,
+       id: formInput['IdNumber'].value,
+       joinDate: formInput['Joineddate'].value,
+       expirationDate: formInput['expDate'].value,
+       bloodGroup: formInput['blood'].value,
     }
 
-    staffInfo.push(newArray)
-    addcards()
-    
-    
 
+    staffDatabase.push(newArray);
+    
+    addCards();
 }
 
-formInput.addEventListener("submit", addToarray); 
+
+function addCards() {
+    let newDom = document.getElementById("mainDiv");
+    
+    // Clear previous cards to avoid duplication
+    newDom.innerHTML = "";
+
+
+    for (let i = 0; i < staffDatabase.length; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "idBody");
+
+        
+        let nameTitle = document.createElement("p");
+        nameTitle.setAttribute("class", "headerTitle");
+        nameTitle.innerHTML = "Name: " + staffDatabase[i].name;
+        newDiv.appendChild(nameTitle);
+
+        let positionTitle = document.createElement("p");
+        positionTitle.innerHTML = "Position: " + staffDatabase[i].position;
+        newDiv.appendChild(positionTitle);
+
+        let idNumber = document.createElement("p");
+        idNumber.innerHTML = "ID Number: " + staffDatabase[i].id;
+        newDiv.appendChild(idNumber);
+
+        let joinDate = document.createElement("p");
+        joinDate.innerHTML = "Joined Date: " + staffDatabase[i].joinDate;
+        newDiv.appendChild(joinDate);
+
+        
+        let expDate = document.createElement("p");
+        expDate.innerHTML = "Expires: " + staffDatabase[i].expirationDate;
+        newDiv.appendChild(expDate);
+
+    
+        let bloodGroup = document.createElement("p");
+        bloodGroup.innerHTML = "Blood Group: " + staffDatabase[i].bloodGroup;
+        newDiv.appendChild(bloodGroup);
+
+        newDom.appendChild(newDiv);
+    }
+}
